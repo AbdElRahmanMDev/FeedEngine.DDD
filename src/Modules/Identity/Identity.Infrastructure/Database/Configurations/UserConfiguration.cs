22ee8cf751e3977a236176132077a606ebdbaaf1
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-28 19:56:01 +0200

    UserConfiguration

--- src/Modules/Identity/Identity.Infrastructure/Database/Configurations/UserConfiguration.cs
+++ src/Modules/Identity/Identity.Infrastructure/Database/Configurations/UserConfiguration.cs
@@ -1,4 +1,6 @@
 ﻿using Identity.Domain.Models;
+using Identity.Domain.Models.enums;
+using Identity.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -8,9 +10,54 @@ namespace Identity.Infrastructure.Database.Configurations
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.ToTable("Users", schema: "Identity");   // Identity.Users
+            builder.ToTable("Users", schema: "Identity");
             builder.HasKey(x => x.Id);
 
+            builder.Property(x => x.Id)
+                .HasConversion(
+                    id => id.Value,
+                    value => UserId.Of(value))
+                .ValueGeneratedNever();
+
+            builder.Property(x => x.Email)
+                .HasMaxLength(100)
+                .HasConversion(x => x.Value, x => Email.Create(x));
+
+            builder.HasIndex(x => x.Email)
+                .IsUnique();
+
+            builder.Property(x => x.Username)
+            .IsRequired()
+            .HasMaxLength(20)
+            .HasConversion(
+                username => username.Value,
+                value => Username.Create(value));
+
+            builder.HasIndex(x => x.Username)
+                .IsUnique();
+
+            builder.Property(x => x.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(500)
+            .HasConversion(
+                hash => hash.Value,
+                value => PasswordHash.FromHash(value));
+
+            builder.ToTable("Users", t =>
+            {
+                t.HasCheckConstraint(
+                    "CK_Users_Status",
+                    "[Status] IN (1, 2, 3)" // Active=1, Suspended=2, Deleted=3
+                );
+            });
+
+            builder.Property(u => u.Status)
+                   .HasConversion<int>()      // enum <-> int
+                   .IsRequired();
+
+            // Optional: default value at DB level
+            builder.Property(u => u.Status)
+                   .HasDefaultValue(AccountStatus.Active);
         }
     }
 }
