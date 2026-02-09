29d87235773d146cf0f1ea57c72e2aa56264b4f5
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-09 20:37:37 +0200

    UserRegisteredDomainEventHandler

---
+++ src/Modules/Identity/Identity.Application/EventHandlers/UserRegisteredDomainEventHandler .cs
@@ -0,0 +1,22 @@
+﻿using Identity.Domain.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Identity.Application.EventHandlers;
+
+public class UserRegisteredDomainEventHandler : INotificationHandler<UserRegisteredDomainEvent>
+{
+
+    private readonly ILogger<UserRegisteredDomainEventHandler> _logger;
+
+    public UserRegisteredDomainEventHandler(
+        ILogger<UserRegisteredDomainEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task Handle(UserRegisteredDomainEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("User registered: {UserId}, {Email}", notification.UserId, notification.Email);
+    }
+}
